5d9f82b2e951cf26fe1f0d7379ff92f1036763fa
yakoo332 <yakupcevik2022@gmail.com>
2025-07-15 03:47:48 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<!-- Schema.org markup -->
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "ProfessionalService",
+  "name": "Buketmedya",
+  "areaServed": [
+    {"@type": "AdministrativeArea", "name": "İstanbul"},
+    {"@type": "AdministrativeArea", "name": "Ankara"},
+    <!-- Tüm iller -->
+  ],
+  "hasOfferCatalog": {
+    "@type": "OfferCatalog",
+    "name": "Dekont Düzenleme Hizmetleri",
+    "itemListElement": [
+      {
+        "@type": "OfferCatalog",
+        "name": "İstanbul",
+        "itemListElement": {
+          "@type": "Offer",
+          "itemOffered": {
+            "@type": "Service",
+            "name": "İstanbul Dekont Düzenleme"
+          }
+        }
+      }
+    ]
+  }
+}
+</script>
